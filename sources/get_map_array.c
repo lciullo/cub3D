d125f504ae008153c146af59ed7471d559f3a448
lciullo <lisa.ciullo42@gmail.com>
2023-09-13 16:57:59 +0200

    update

--- sources/get_map_array.c
+++ sources/get_map_array.c
@@ -1,24 +1,21 @@
 #include "cub3D.h"
-
+#include <stdio.h>
 int read_map(char *path)
 {
-    char    *tmp;
-    int     fd;
-    
+	char	*tmp;
+	int	fd;
 
-    tmp = NULL;
-    fd = open(path, O_RDONLY);
-	if (fd == FAILURE)
+	tmp = NULL;
+	fd = open("", O_RDONLY);
+	if (fd == -1)
 		return (ft_dprintf(2, "Error\nThe file could not be opened\n"), FAILURE);
-    //ft_dprintf(2, "line : %s\n", tmp);
-    while (1)
+	while (1)
 	{
 		tmp = get_next_line(fd);
-        //ft_dprintf(2, "line : %s\n", tmp);
 		if (tmp == NULL)
 			break ;
-		free(tmp);
+		ft_dprintf(1, "line : %s\n", tmp);
 	}
 	close(fd);
-    return (SUCCESS);
+	return (SUCCESS);
 }
