0eee295877a7f6fcabfd7191554db7f8746531d0
clem <llovio.clemence@gmail.com>
2023-09-21 13:24:00 +0200

    just so i can pull

--- sources/main.c
+++ sources/main.c
@@ -4,19 +4,30 @@ int	main(int ac, char **av)
 {
 	t_data		data;
 	t_parsing	utils;	
-	t_data	data;
-  
+
+	int	y;
+
+	y = 0;
 	(void)av;
 	if (ac != 2)
 	{
 		ft_dprintf(2, "Error\nWrong number of arguments\n");
 		return (FAILURE);
 	}
-	if (launch_mlx(&data) == FAILURE)
-	init_struct(&data, &utils);
 	if (parsing(av[1], &data, &utils) == FAILURE)
 		return (FAILURE);
-	/*if (launch_mlx(&data, &draw) == FAILURE)
-		return (FAILURE);*/
+	int x = 0;
+	while (data.map[y])
+	{
+		x = 0;
+		while (data.map[y][x])
+		{
+			ft_dprintf(1, "%c", data.map[y][x]);
+			x++;
+		}
+		y++;
+	}
+	// if (launch_mlx(&data) == FAILURE)
+	// 	return (FAILURE);
 	return (0);
 }
