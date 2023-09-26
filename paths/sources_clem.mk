589506c42607a805511e563c7f64cda2a59e15f4
Clemence Llovio <cllovio@z3r3p3.42lyon.fr>
2023-09-26 08:45:17 +0200

    je peux pas push sinon

--- paths/sources_clem.mk
+++ paths/sources_clem.mk
@@ -4,7 +4,10 @@ SRCS	+=	$(DIR_SRCS)hook.c\
 			$(DIR_SRCS)mini_map.c\
 			$(DIR_SRCS)quit.c\
 			$(DIR_SRCS)utils.c\
+<<<<<<< Updated upstream
 			$(DIR_SRCS)draw/draw_direction_vector.c\
+=======
+>>>>>>> Stashed changes
 			$(DIR_SRCS)draw/my_mlx_pixel_put.c\
 			$(DIR_SRCS)draw/draw_square.c\
 			$(DIR_SRCS)draw/draw_rectangle.c\
